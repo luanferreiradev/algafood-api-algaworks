b863a95bf7a682bfed2b557bca207c6e00694f6b
Luan Lara <122845430+LuanLaraDev@users.noreply.github.com>
2025-11-01 20:41:35 -0300

    Desambiguação de beans com @Primary

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -5,21 +5,16 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        for (Notificador notificador : notificadores) {
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }
-
+        this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
 
 }
